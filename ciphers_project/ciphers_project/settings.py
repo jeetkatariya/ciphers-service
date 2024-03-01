3f5ba911ee9517add527ab826de4ffa6e1001761
jkkatari <jkkatari@iu.edu>
2024-02-29 19:44:52 -0500

    Updatted settings.py with extra cors url

--- ciphers_project/ciphers_project/settings.py
+++ ciphers_project/ciphers_project/settings.py
@@ -127,4 +127,4 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = ["https://jeetkatariya.github.io",
-                        "http://localhost:3000"]
+                        "http://localhost:3000", "https://ciphers-service-jeetkatariya.onrender.com"]
